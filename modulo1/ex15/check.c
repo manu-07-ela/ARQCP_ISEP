48a9c808653dc01d4a0f38719c8a872191ec91d1
Manuela Leite <1200720@isep.ipp.pt>
2021-10-27 10:15:54 +0100

    Exercício 15 - Mósulo 1

---
+++ modulo1/ex15/check.c
@@ -0,0 +1,6 @@
+ int check ( int x, int y, int z){
+	if ( x<y && y<z){
+		return 1;
+	}
+	return 0;
+ }
---
+++ modulo1/ex15/populate.c
@@ -0,0 +1,7 @@
+#include <stdlib.h>
+void populate( int *vec , int num, int limit){
+	
+	for(int i=0; i<num; i++){
+		*(vec+i) = rand() % (limit+1);
+	}
+}
