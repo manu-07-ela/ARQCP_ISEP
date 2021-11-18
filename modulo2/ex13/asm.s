391ef82886cc880e1273523bda36ee4a5a9b9595
Manuela Leite <1200720@isep.ipp.pt>
2021-11-18 14:45:04 +0000

    Exercício 13 - Módulo 2

---
+++ modulo2/ex13/asm.s
@@ -0,0 +1,21 @@
+.section .data
+    .global base # int - l
+	.global height # int - l
+
+.section .text
+    .global getArea
+
+    getArea:
+    
+    movq $0, %rax
+    movl base(%rip), %eax
+    movl height(%rip), %ecx
+    mull %ecx 
+    movl $0, %edx
+    movl $0, %ecx
+    movl $2, %ecx
+    divl %ecx
+    
+    ret
+
+
--- modulo2/ex13/main.c
+++ modulo2/ex13/main.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 #include "asm.h"
 
-int op1 = 2147483647;
-int op2 = 1;
+int base = 4;
+int height = 5;
 
 int main(void) {
 
-	char res = test_flags();
+	int res = getArea();
 	printf("%d\n",res);
 	
 	return 0;
